901cce78d695694927289cacabd0e60ef5978c99
SheelaKhinchi <khinchisheela@gmail.com>
2024-07-31 21:37:58 +0530

    Initial commit

---
+++ my-tailwind-project/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust this path to your project's structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
